b6fdc4a85ee9556a6bc4ee5c7495df3beb65f8b7
rankiandras <rankiandras@gmail.com>
2022-04-25 12:20:49 +0200

    setTimout in Footer2 changed

--- src/components/Footer2.jsx
+++ src/components/Footer2.jsx
@@ -12,17 +12,18 @@ class Footer2 extends Component {
 
   componentDidUpdate() {
     setTimeout(() => {
-      if (document.getElementById('footer').classList.contains('invisible')) {
+      this.setState({ key1: "footer"})
+      /* if (document.getElementById('footer').classList.contains('invisible')) {
         document.getElementById('footer').classList.remove('invisible')
         document.getElementById('footer').classList.add('footer')
-      }
+      } */
     }, 2000)
   }
 
 
   render() {
     return (
-      <div id='footer' className={this.state.key1}
+      <div /* id='footer' */ className={this.state.key1}
       onClick={
         () => this.setState({ key1: "invisible"})}>{this.props.date}</div>
     )
